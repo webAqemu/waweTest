b6a790746ea2e4f265037e7a761fdb2766c225da
Misha <notych2812@gmail.com>
2021-06-19 02:17:32 +0300

    fixed bugs v2.0

--- js/script.js
+++ js/script.js
@@ -9,14 +9,61 @@ $(document).ready(function () {
 });
 
 document.addEventListener("click", function (e) {
-  const body = document.querySelector("html");
   const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".header__nav");
+  const nav = document.querySelector(".header__list");
   const layer = document.querySelector(".layer");
+  const videoPopup = document.querySelector(".video__popup");
   if (e.target == burger || e.target == layer || e.target.classList.contains("header__link")) {
     burger.classList.toggle("active");
     nav.classList.toggle("active");
     layer.classList.toggle("active");
-    body.classList.toggle("active");
+  }
+
+  if (e.target == layer) {
+    burger.classList.remove("active");
+    nav.classList.remove("active");
+    layer.classList.remove("active");
+    videoPopup.classList.remove("active");
+    if (document.querySelector(".gallery__popup")) {
+      document.querySelector(".gallery__popup").remove();
+    }
+  }
+
+  if (e.target.classList.contains("gallery__btn")) {
+    const filter = e.target.dataset.filter;
+    const items = document.querySelectorAll(".gallery__item");
+
+    document.querySelector(".gallery__btn.active").classList.remove("active");
+    e.target.classList.add("active");
+    items.forEach((item) => item.classList.remove("hidden"));
+    if (!(filter == "all")) {
+      items.forEach((item) => {
+        if (!(item.dataset.filter == filter)) {
+          item.classList.add("hidden");
+        }
+      });
+    }
+  }
+
+  if (e.target.classList.contains("video__play")) {
+    videoPopup.classList.add("active");
+    layer.classList.add("active");
+  }
+
+  if (e.target.classList.contains("gallery__item")) {
+    const imgSrc = e.target.querySelector("img").getAttribute("src");
+    const template = `
+    <div class="gallery__popup">
+      <button class="gallery__close">закрыть</button>
+      <img src="${imgSrc}" />
+    </div>
+    `;
+    e.target.insertAdjacentHTML("beforeend", template);
+    layer.classList.add("active");
+  }
+
+  if (e.target.classList.contains("gallery__close")) {
+    e.target.parentElement.remove();
+    layer.classList.remove("active");
   }
 });
